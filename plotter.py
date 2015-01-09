d555cfefb4f2b6b009c93e205d80dfd941dc9312
atwel <atwell.jonathan@gmail.com>
2015-01-09 14:56:17 -0500

    improved version that automates more

--- plotter.py
+++ plotter.py
@@ -26,114 +26,115 @@ def get_step_count(PRODUCT_TYPES):
     return STEPS
 
 
-x = [3,4,5,6,7,8,9]
-plt.axis([1,10,-.05,1.1])
 
-PRODUCT_TYPES = [3,4,5,6,7,8,9]
+
+PRODUCT_TYPES = [2,3,4,5,6,7,8,9]
 CHEMISTRY = ["ALL"] # "SOLOH"
 INTEL_TYPE = [False] # => "selective"
 URN_TYPE = ["fixed-rich-target","fixed-poor-target", "fixed-rich-source","fixed-poor-source", "endo-rich-source","endo-poor-source"]
 TOPOLOGY = ["spatial"] # "well-mixed"
+GRAPH_TYPES = ["Cycles Alive","3+ Rules Alive", "Cell Count"]
 
-stack = {}
-#graph_type = "Cycles Alive"
-#graph_type = "3+ Cycles Alive"
-#graph_type = "3+ Rules Alive"
-graph_type = "Cell Count"
-max = 0
-for TYPES in PRODUCT_TYPES:
-	for CHEM in CHEMISTRY:
-		for INTEL in INTEL_TYPE:
-			for URN in URN_TYPE:
-				for TOPO in TOPOLOGY:
-					mystr = "-".join([str(TYPES), CHEM, str(INTEL), URN, TOPO])
-					datafile = open(mystr+".csv","r+")
-					vals = []
-					three_vals = []
-					cells = []
-					rules = []
-					count_runs = 0.
+x = [2,3,4,5,6,7,8,9]
+for GRAPH in GRAPH_TYPES:
+	stack = {}
+	for TYPES in PRODUCT_TYPES:
+		for CHEM in CHEMISTRY:
+			for INTEL in INTEL_TYPE:
+				for URN in URN_TYPE:
+					for TOPO in TOPOLOGY:					
+						mystr = "-".join([str(TYPES), CHEM, str(INTEL), URN, TOPO])
+						#print mystr
+						datafile = open(mystr+".csv","r+")
+						vals = []
+						three_vals = []
+						cells = []
+						rules = []
+						count_runs = 0.
+						cnt = 0.
+						step_count = get_step_count(TYPES)
 					
-					step_count = get_step_count(TYPES)
-					for line in datafile:
-
-						pre = line.replace("{","|").replace("}","|")
-						raw = pre.strip().split("|")
-						run = int(raw[0].replace(",",""))
-						precycles = raw[1].split(",")
-						cycles = {}
-						for i in precycles:
-							try:
-								j,k = i.split(":")
-								cycles[int(j)] = int(k)
-							except:
-								cycles = {}
-
-						after = raw[2].split(",")
-
-						if graph_type == "Cycles Alive":
+						for line in datafile:
 							count_runs += 1
-							add_info = "Fraction of 100 runs w/ Hypercycles"			
-							if int(after[4]) > step_count*.95:
-								if len(cycles.keys()) > 0:
-									vals.append(1)
-								else:
+							pre = line.replace("{","|").replace("}","|")
+							raw = pre.strip().split("|")
+							run = int(raw[0].replace(",",""))
+							precycles = raw[1].split(",")
+							cycles = {}
+							for i in precycles:
+								try:
+									j,k = i.split(":")
+									cycles[int(j)] = int(k)
+								except:
+									cycles = {}
+
+							after = raw[2].split(",")
+
+							if GRAPH == "Cycles Alive":			
+								if int(after[4]) > step_count*.95:
+									if len(cycles.keys()) > 0:
+										vals.append(1)
+									else:
+										vals.append(0)
+
+							elif GRAPH == "3+ Rules Alive":
+								if after[2]=='False':
 									vals.append(0)
-
-						elif graph_type == "3+ Cycles Alive":
-							count_runs += 1
-							add_info = "Fraction of 100 runs w/ 3+ cycles"
-							if cycles != {}:
-								if len(cycles.keys()) > 1:
-									vals.append(1)
 								else:
-									vals.append(0)
-							else:		
-								vals.append(0)
+									vals.append(1)
 
-						elif graph_type == "3+ Rules Alive":
-							count_runs += 1
-							add_info = "Fraction of 100 runs w/ 3+ Rules in Cycle"
-							if after[2]=='False':
-								vals.append(0)
-							else:
-								vals.append(1)
-
-						elif graph_type == "Cell Count":
-							add_info = "Average # cells alive, if alive"
-							
-							if int(after[4]) > step_count*.95:
-								if len(cycles.keys()) > 0:
-									a = int(after[3])
-									vals.append(a)
-									count_runs += 1
-									if a > max:
-										print "new max", a
-										max = a
-							plt.axis([1,10,-0.5,max])
-					if count_runs == 0:
-						count_runs = 1 #because numerator is 0 anyway
-							
-							
-
-              				try:
-              					stack[URN].append(sum(vals)/count_runs)
-              				except:
-              					stack[URN] = [sum(vals)/count_runs]
-
-print stack
-
-plt.plot(x,stack["fixed-rich-source"], label="source-rich", color="k", marker="s",markeredgecolor="k",ms=8,linestyle="solid", linewidth=2)
-plt.plot(x,stack["fixed-poor-source"], label="souce-poor", color="k", marker="^",linestyle="solid",linewidth=2,markeredgecolor="k",ms=8)
-plt.plot(x,stack["endo-rich-source"], label="stigmergy-rich", color="r", marker="s",linestyle="solid",linewidth=2,markeredgecolor="r",ms=8)
-plt.plot(x,stack["endo-poor-source"], label="stigmergy-poor", color="r", marker="^",linestyle="solid",linewidth=2,markeredgecolor="r",ms=8)
-plt.plot(x,stack["fixed-rich-target"], label="target-rich", color="b", marker="s",linestyle="solid",linewidth=2,markeredgecolor="b",ms=8)
-plt.plot(x,stack["fixed-poor-target"], label="target-poor", color="b", marker="^",linestyle="solid",linewidth=2,markeredgecolor="b",ms=8)
-#plt.plot(x,nonspatial, label="nonspatial", color="g", marker="s",linestyle="solid",linewidth=2,markeredgecolor="g",ms=8)
+							elif GRAPH == "Cell Count":
+								if int(after[4]) > step_count*.95:
+									if len(cycles.keys()) > 0:
+										vals.append(int(after[3]))
+										cnt += 1
+								count_runs = cnt
 
 
-plt.title("ALL Chem - No Pass Back - " + graph_type)
-plt.ylabel(add_info)
-plt.xlabel("Types of Products in Chemistry")
-legend = plt.legend(bbox_to_anchor=(.01, -0.3, 1., .102), loc=3,ncol=3, mode="expand", borderaxespad=1.)
-plt.savefig(graph_type, bbox_extra_artists=(legend,), bbox_inches='tight')
+              					try:
+              						if count_runs == 0:
+              							stack[URN].append(0)
+              						else:
+              							stack[URN].append(sum(vals)/count_runs)
+              					except:
+              						stack[URN] = [sum(vals)/count_runs]
+
+
+	if GRAPH == "Cycles Alive":
+		x = [2,3,4,5,6,7,8,9]
+		plt.axis([1,10,-.05,1.1])
+		plt.plot(x,stack["fixed-rich-source"], label="source-rich", color="k", marker="s",markeredgecolor="k",ms=8,linestyle="solid", linewidth=2)
+		plt.plot(x,stack["fixed-poor-source"], label="souce-poor", color="k", marker="^",linestyle="solid",linewidth=2,markeredgecolor="k",ms=8)
+		plt.plot(x,stack["endo-rich-source"], label="stigmergy-rich", color="r", marker="s",linestyle="solid",linewidth=2,markeredgecolor="r",ms=8)
+		plt.plot(x,stack["endo-poor-source"], label="stigmergy-poor", color="r", marker="^",linestyle="solid",linewidth=2,markeredgecolor="r",ms=8)
+		plt.plot(x,stack["fixed-rich-target"], label="target-rich", color="b", marker="s",linestyle="solid",linewidth=2,markeredgecolor="b",ms=8)
+		plt.plot(x,stack["fixed-poor-target"], label="target-poor", color="b", marker="^",linestyle="solid",linewidth=2,markeredgecolor="b",ms=8)
+	
+	elif GRAPH == "3+ Rules Alive":
+		x = [3,4,5,6,7,8,9]
+		plt.axis([1,10,-.05,1.1])
+		plt.plot(x,stack["fixed-rich-source"][1:], label="source-rich", color="k", marker="s",markeredgecolor="k",ms=8,linestyle="solid", linewidth=2)
+		plt.plot(x,stack["fixed-poor-source"][1:], label="souce-poor", color="k", marker="^",linestyle="solid",linewidth=2,markeredgecolor="k",ms=8)
+		plt.plot(x,stack["endo-rich-source"][1:], label="stigmergy-rich", color="r", marker="s",linestyle="solid",linewidth=2,markeredgecolor="r",ms=8)
+		plt.plot(x,stack["endo-poor-source"][1:], label="stigmergy-poor", color="r", marker="^",linestyle="solid",linewidth=2,markeredgecolor="r",ms=8)
+		plt.plot(x,stack["fixed-rich-target"][1:], label="target-rich", color="b", marker="s",linestyle="solid",linewidth=2,markeredgecolor="b",ms=8)
+		plt.plot(x,stack["fixed-poor-target"][1:], label="target-poor", color="b", marker="^",linestyle="solid",linewidth=2,markeredgecolor="b",ms=8)
+
+	elif GRAPH == "Cell Count":
+		x = [2,3,4,5,6,7,8,9]
+		plt.axis([1,10,-.5,14.5])
+		plt.plot(x,stack["fixed-rich-source"], label="source-rich", color="k", marker="s",markeredgecolor="k",ms=8,linestyle="solid", linewidth=2)
+		plt.plot(x,stack["fixed-poor-source"], label="souce-poor", color="k", marker="^",linestyle="solid",linewidth=2,markeredgecolor="k",ms=8)
+		plt.plot(x,stack["endo-rich-source"], label="stigmergy-rich", color="r", marker="s",linestyle="solid",linewidth=2,markeredgecolor="r",ms=8)
+		plt.plot(x,stack["endo-poor-source"], label="stigmergy-poor", color="r", marker="^",linestyle="solid",linewidth=2,markeredgecolor="r",ms=8)
+		plt.plot(x,stack["fixed-rich-target"], label="target-rich", color="b", marker="s",linestyle="solid",linewidth=2,markeredgecolor="b",ms=8)
+		plt.plot(x,stack["fixed-poor-target"], label="target-poor", color="b", marker="^",linestyle="solid",linewidth=2,markeredgecolor="b",ms=8)
+
+
+
+	plt.title("ALL Chem - Simultaneous Pass, select cell - " + GRAPH)
+	plt.ylabel("Fraction of 100 runs w/ Hypercycles")
+	plt.xlabel("Types of Products in Chemistry")
+	legend = plt.legend(bbox_to_anchor=(.01, -0.3, 1., .102), loc=3,ncol=3, mode="expand", borderaxespad=1.)
+	plt.savefig(GRAPH, bbox_extra_artists=(legend,), bbox_inches='tight')
+	plt.cla()
